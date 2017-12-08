47f06f716acb7f4d8bb20d7bd58d645d1da4fd57
r.ron <r.ron@gl-f.com>
2017-12-08 10:46:32 +0700

    add comment

--- day1/xml-config/src/main/java/com/memorynotfound/controller/HomeController.java
+++ day1/xml-config/src/main/java/com/memorynotfound/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = {"/","/hello"}, method = RequestMethod.GET)
     public String index(ModelMap model){
         model.addAttribute("message", "Spring MVC XML Config Example");
         return "index";
